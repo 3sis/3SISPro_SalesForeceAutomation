cf4c71cc2b8b69f42797bb852176834b40eae9e0
krishna <krishnawaghmode1995@gmail.com>
2022-11-04 22:02:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h2 style="color:cyan">Project Installation</h2>
+
+   In the XAMPP directory, there exists a folder called “htdocs”. This is where all the programs for the web pages will be stored.
+
+Now, to run a PHP script:
+
+Go to “C:\xampp\htdocs” and inside it, create a folder. Let’s call it “demo”. It’s considered good practice to create a new folder for every project you work on.
+Clone the Project: 
+```bash
+     git clone https://github.com/krishnawaghmode/laravel_demo.git
+     cd laravel_demo
+     composer install
+     cp .env.example .env
+
+     Set up .env file
+
+     php artisan key:generate
+
+     http://localhost/project/laravel_demo
+ ```    
+     
+   
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Sales Admin | CORK - Multipurpose Bootstrap Dashboard Template </title>
+    <link rel="icon" type="image/x-icon" href="{{asset('public/assets/img/favicon.ico')}}"/>
+    <link href="{{asset('public/assets/css/loader.css')}}" rel="stylesheet" type="text/css" />
+    <script src="{{asset('public/assets/js/loader.js')}}"></script>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{asset('public/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('public/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="{{asset('public/plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('public/assets/css/dashboard/dash_1.css')}}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <!--Table css -->
+    <link href="{{asset('public/assets/css/tables/table-basic.css')}}" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" type="text/css" href="{{asset('public/plugins/table/datatable/datatables.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('public/plugins/table/datatable/dt-global_style.css')}}">
+    <!-- END Table-->
+
+    <!-- css3SIS -->
+     <link rel="stylesheet" type="text/css" href="{{asset('public/assets/css3SIS/dataTable3SIS.css')}}">
+     <link rel="stylesheet" type="text/css" href="{{asset('public/assets/css3SIS/button3SIS.css')}}">
+      <!-- <link href="{{asset('public/assets/css/main.css')}}" rel="stylesheet" type="text/css" /> -->
+    <!-- END css3SIS -->
+
+    <!--  BEGIN CUSTOM STYLE FILE  -->
+    <link href="{{asset('public/assets/css/scrollspyNav.css')}}" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{asset('public/plugins/select2/select2.min.css')}}">
+    <!--  END CUSTOM STYLE FILE  -->
+
+    @yield('css')
+</head>
+<body>
+    <!-- BEGIN LOADER -->
+    <div id="load_screen"> <div class="loader"> <div class="loader-content">
+        <div class="spinner-grow align-self-center"></div>
+    </div></div></div>
+    <!--  END LOADER -->
+
+      @include('inc.navbar')
+
+    <!--  BEGIN MAIN CONTAINER  -->
+    <div class="main-container" id="container">
+
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+       @include('inc.sidebar')
+        
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+          
+          @yield('content')
+          
+        </div>
+        <!--  END CONTENT AREA  -->
+
+    </div>
+    <!-- END MAIN CONTAINER -->
+
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="{{asset('public/assets/js/libs/jquery-3.1.1.min.js')}}"></script>
+    <script src="{{asset('public/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{asset('public/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('public/plugins/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{asset('public/assets/js/app.js')}}"></script>
+
+    <script src="{{asset('public/assets/js/custom.js')}}"></script>
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+    <script src="{{asset('public/plugins/apex/apexcharts.min.js')}}"></script>
+    <script src="{{asset('public/assets/js/dashboard/dash_1.js')}}"></script>
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+
+    <!-- BEGIN DataTable -->
+    <script src="{{asset('public/plugins/table/datatable/datatables.js')}}"></script>
+
+    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+    <script src="{{asset('public/assets/js/scrollspyNav.js')}}"></script>
+    <script src="{{asset('public/plugins/select2/select2.min.js')}}"></script>
+    <script src="{{asset('public/plugins/select2/custom-select2.js')}}"></script>
+    
+    <script src="{{asset('public/plugins/bootstrap-maxlength/bootstrap-maxlength.js')}}"></script>
+    <script src="{{asset('public/plugins/bootstrap-maxlength/custom-bs-maxlength.js')}}"></script>
+
+    <link href="{{asset('public/assets/css/components/tabs-accordian/custom-tabs.css')}}" rel="stylesheet" type="text/css" />
+    <script src="{{asset('public/assets/js/common3SIS/validation3SIS.js')}}"></script>
+
+    <script>
+        $(document).ready(function() {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+            App.init();
+            let current_route = '<?php echo Request::path()?>';
+            console.log(current_route);
+            if(current_route != ''){
+
+            $.ajax({
+                    type : 'post',
+                    url : "{{url('current_url')}}",
+                    data:{'current_url':current_route},
+                    success:function(response){
+                     console.log(response.status);
+
+                     if(response.status == 'success'){
+                        console.log(response.data);
+                        // current_url
+
+                        $('#current_url').html(response.data);
+                     }else{
+                        // alert("current_url not found ?");
+                        return false;
+                     }
+                    }
+                });
+    }
+});
+    </script>
+
+ @yield('js_code')
+
+    <script type="text/javascript">
+        $('#add_Data').click(function () {
+            $('#entryModalSmall').modal('show');
+        });
+    </script>  
+</body>
+</html>
